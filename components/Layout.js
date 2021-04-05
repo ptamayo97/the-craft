49c823738ecf1949a6333c91bfbbacb9a71463cf
ptamayo <ptamayo97@yahoo.com>
2021-04-05 16:25:51 -0700

    added custom font

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,15 @@
 import React, { Component, Fragment } from "react";
+import { Head } from "next/head";
 import "../styles/Home.module.css";
 import NavBar from "./NavBar";
 
 export class Layout extends Component {
   render() {
+    const styles = {
+      background: "#011724",
+    };
     return (
-      <div className="dark">
+      <div style={styles}>
         <NavBar />
         {this.props.children}
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: Bebas Neue;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </Layout>
   );
 }
